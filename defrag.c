3b4d2a4ab263665be77f7aad9ca86f1a25a7895e
Sarah Depew <sdepew@brynmawr.edu>
2018-04-12 15:41:43 -0400

    Reversing IDirect To File

--- defrag.c
+++ defrag.c
@@ -460,7 +460,7 @@ void outputIFile(inode *fileToOutputOriginal, inode *fileToOutputNew, int size, 
     //calculate number of blocks total and number of indirect layers required to get those blocks...
     divisionResult = ((float) numBlocks) / ((float) (size) / (float) (sizeof(int)));
     long numIndirect = ceilf(divisionResult);
-    printIBlocks(numIndirect, numBlocks, fileToOutputOriginal->iblocks, dataRegionOld, size, oldOutput);
+//    printIBlocks(numIndirect, numBlocks, fileToOutputOriginal->iblocks, dataRegionOld, size, oldOutput);
 
     //read and output new file's data blocks
 
@@ -472,7 +472,7 @@ void outputIFile(inode *fileToOutputOriginal, inode *fileToOutputNew, int size, 
     //calculate number of blocks total and number of indirect layers required to get those blocks...
     divisionResult = ((float) numBlocks) / ((float) (size) / (float) (sizeof(int)));
     numIndirect = ceilf(divisionResult);
-    printIBlocks(numIndirect, numBlocks, fileToOutputNew->iblocks, dataRegionNew, size, newOutput);
+//    printIBlocks(numIndirect, numBlocks, fileToOutputNew->iblocks, dataRegionNew, size, newOutput);
 
 //    free(outputOldFileName);
 //    free(outputNewFileName);
