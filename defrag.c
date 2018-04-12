e40f812c7ff3397e9b04554f5d959b783a6fd948
Sarah Depew <sdepew@brynmawr.edu>
2018-04-12 17:28:37 -0400

    Fixing Code

--- defrag.c
+++ defrag.c
@@ -202,10 +202,11 @@ boolean defragment(char *inputFile) {
         //Write new inode region! and write the entire file!
         fwrite(bootBlockPtr, SIZEOFBOOTBLOCK, 1, finalOutputPtr);
         fwrite(superblockPtr, SIZEOFSUPERBLOCK, 1, finalOutputPtr);
-        fwrite((((void *) superblockPtr) + SIZEOFSUPERBLOCK), superblockPtr->data_offset * size, 1,
+        fwrite(((void *) superblockPtr + SIZEOFSUPERBLOCK), (superblockPtr->inode_offset * size), 1, finalOutputPtr);
+        fwrite((((void *) superblockPtr) + SIZEOFSUPERBLOCK + (superblockPtr->inode_offset * size)), superblockPtr->data_offset * size, 1,
                finalOutputPtr);
         fwrite(allOfDataRegion, outputFileSize, 1, finalOutputPtr);
-//        fwrite((((void *) superblockPtr) + SIZEOFSUPERBLOCK), superblockPtr->data_offset * size, 1, finalOutputPtr);
+        fwrite((dataBlockPtr + (currentDataBlock * size)), (superblockPtr->swap_offset - currentDataBlock) * size, 1, finalOutputPtr);
 
         //TODO: add swap region and add free blocks, here
 
