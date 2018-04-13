1dd66d4a093b62734e7f6c98127914c22a4e0cc0
Sarah Depew <sdepew@brynmawr.edu>
2018-04-13 07:33:45 -0400

    Printing Inodes

--- defrag.c
+++ defrag.c
@@ -231,7 +231,7 @@ boolean defragment(char *inputFile) {
         printf("Final Print\n");
         printf("head of inode list %d\n", superblockPtr->free_inode);
         printf("value of currentDataBlock %ld\n", currentDataBlock);
-//        printInodes(inodePtr, dataBlockPtr, size, superblockPtr->inode_offset, superblockPtr->data_offset);
+        printInodes(inodePtr, dataBlockPtr, size, superblockPtr->inode_offset, superblockPtr->data_offset);
         printf("head of free list %d\n", superblockPtr->free_block);
 //        printDataBlocks(dataBlockPtr, size, superblockPtr->data_offset, superblockPtr->swap_offset);
 
